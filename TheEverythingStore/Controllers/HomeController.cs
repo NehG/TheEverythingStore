703c25aa6232aa0a640ab945e2c0d03317d567fa
ifotn <rich@infrontofthenet.com>
2019-01-30 13:51:19 -0500

    end lesson4. sql server enabled

--- TheEverythingStore/Controllers/HomeController.cs
+++ TheEverythingStore/Controllers/HomeController.cs
@@ -9,6 +9,9 @@ namespace TheEverythingStore.Controllers
 {
     public class HomeController : Controller
     {
+        // connect to db
+        private DbModel db = new DbModel();
+
         public ActionResult Index()
         {
             return View();
@@ -42,14 +45,17 @@ namespace TheEverythingStore.Controllers
             // pass mock products to the view for display
             ViewBag.Products = products; */
 
-            var products = new List<Product>();
+            //var products = new List<Product>();
 
-            for (int i = 1; i <= 10; i++)
-            {
-                Product product = new Product();
-                product.Name = "Product " + i.ToString();
-                products.Add(product);
-            }
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Product product = new Product();
+            //    product.Name = "Product " + i.ToString();
+            //    products.Add(product);
+            //}
+
+            // use the Product model to retrieve the entire product list from sql server
+            var products = db.Products.ToList();
 
             // load the view and pass the product list to it
             return View(products);
